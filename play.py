64848d8613e1b6d0cd9d73ae0b2c4bf028310e92
TshAkEAb <64094722+TshAkEAb@users.noreply.github.com>
2021-03-04 20:31:00 +0300

    Update play.py

--- play.py
+++ play.py
@@ -24,8 +24,40 @@ def updateMsgs(client, message,redis):
   title = message.chat.title
   rank = isrank(redis,userID,chatID)
   text = message.text
+  ADDed = """◀️꒐ بواسطه ⌁ {} 
+  ✅꒐ {} بالفعل مفعله في المجموعة"""
+  ADD = """◀️꒐ بواسطه ⌁ {} 
+  ✅꒐ تم تفعيل {} في المجموعة"""
+  unADDed = """◀️꒐ بواسطه ⌁ {} 
+  ❎꒐ {} بالفعل معطله في المجموعة"""
+  unADD = """◀️꒐ بواسطه ⌁ {} 
+  ❎꒐ تم تعطيل {} في المجموعة"""
+
+  if (rank is not False or rank is not  0 or rank != "vip"):
+
+    if text == "تفعيل الالعاب" :
+      R = text.split(" ")[1]
+      get = redis.sismember("{}Nbot:gpgames".format(BOT_ID),chatID)
+      BY = "<a href=\"tg://user?id={}\">{}</a>".format(userID,userFN)
+      if get :
+        save = redis.srem("{}Nbot:gpgames".format(BOT_ID),chatID)
+        Bot("sendMessage",{"chat_id":chatID,"text":ADD.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+      else:
+         Bot("sendMessage",{"chat_id":chatID,"text":ADDed.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+
+    if text == "تعطيل الالعاب" :
+      R = text.split(" ")[1]
+      BY = "<a href=\"tg://user?id={}\">{}</a>".format(userID,userFN)
+      get = redis.sismember("{}Nbot:gpgames".format(BOT_ID),chatID)
+      if get :
+        Bot("sendMessage",{"chat_id":chatID,"text":unADDed.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+      else:
+        save = redis.sadd("{}Nbot:gpgames".format(BOT_ID),chatID)
+        Bot("sendMessage",{"chat_id":chatID,"text":unADD.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+
+  
   games = {"rps.py":"🧱📃✂️","xo.py":"❌ ⭕️"}
-  if text and re.search("^الالعاب$|^العاب$",text):
+  if text and re.search("^الالعاب$|^العاب$",text) and not redis.sismember("{}Nbot:gpgames".format(BOT_ID),chatID):
     tx = "🕹꒐ اليك الالعاب المقدمه من (<a href=\"http://t.me/zx_xx\">TshakeTeam</a>)"
     onlyfiles = [f for f in listdir("files") if isfile(join("files", f))]
     array = []
@@ -41,5 +73,3 @@ def updateMsgs(client, message,redis):
     points = (redis.hget("{}Nbot:{}:points".format(BOT_ID,chatID),userID) or 0)
     Bot("sendMessage",{"chat_id":chatID,"text":"🔢꒐ نقاطك :- ({})".format(points),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
 
-def updateCb(client, callback_query,redis):
-  pass
